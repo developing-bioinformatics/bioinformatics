b0ee729100ff8b867328f36508b0168583a9938a
robert harbert <rharbert@stonehill.edu>
2021-02-21 00:13:24 -0500

    update algorithms

---
+++ files/2021_notes/shell1.sh
@@ -0,0 +1,30 @@
+# shell workshop notes
+
+#print working directory (folder)
+pwd 
+
+# ls 'list files' -- what's here?
+ls
+ls -l
+
+# cd 'change directory' -- let you move around the file system
+cd /projectnb/ct-shbioinf/rharbert/bioinformatics
+
+# man 'manual pages' -- give info
+man ls
+
+# cat -- print contents of a file
+cat shell1.sh
+
+cd .. # go to enclosing folder 
+
+cd # sends you to user home directory
+
+cd data
+ls *.R # * is a wildcard character to help search 
+#??
+# how can you see what to expect with *
+echo *.R # kind of like print() 
+
+history
+
---
+++ files/2021_notes/shell_workshop_data.sh
@@ -0,0 +1,3 @@
+# shell script to perform BLAST query with practice data from the Data Carpentry Shell-Genomics workshop
+# data: https://ndownloader.figshare.com/articles/7726454/versions/2
+# curl https://ndownloader.figshare.com/articles/7726454/versions/2
